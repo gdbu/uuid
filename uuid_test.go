f14e803b638ea82a827c831f873ed3b13d4341ee
Josh Montoya <josh@montoya.io>
2018-08-13 12:21:59 -0700

    Improve commenting

--- uuid_test.go
+++ uuid_test.go
@@ -15,19 +15,23 @@ func TestUUID(t *testing.T) {
 
 	ts.UUID = newUUID(6)
 
+	// Attempt to marshal the test struct
 	if bs, err = json.Marshal(ts); err != nil {
 		t.Fatalf("Error marshaling: %v", err)
 	}
 
+	// Attempt to unmarshal the marshaled bytes
 	if err = json.Unmarshal(bs, &nts); err != nil {
 		t.Fatalf("Error unmarshaling: %v", err)
 	}
 
+	// Ensure test struct matches new test struct
 	if ts != nts {
 		t.Fatalf("Invalid value, expected %v and received %v", ts, nts)
 	}
 }
 
+// testStruct is used to simulate a struct including a UUID
 type testStruct struct {
 	UUID UUID `json:"uuid"`
 }
