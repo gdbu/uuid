94da945e7ae39eec671af9f193e63691801bac94
Josh Montoya <josh@montoya.io>
2021-01-06 16:10:47 -0800

    implement test suite for IsZero

--- uuid_test.go
+++ uuid_test.go
@@ -39,7 +39,7 @@ func TestUUID(t *testing.T) {
 	}
 }
 
-func TestUUIDString(context *testing.T) {
+func TestUUID_String(t *testing.T) {
 	var (
 		id                     UUID
 		randomPrefix           string
@@ -53,13 +53,26 @@ func TestUUIDString(context *testing.T) {
 	id = newUUID(6)
 	randomPrefix = strings.Split(id.String(), expectedSuffix)[0]
 
-	test := simply.Target(id.String(), context, "Test standard UUID format")
+	test := simply.Target(id.String(), t, "Test standard UUID format")
 	result := test.Equals(randomPrefix + expectedSuffix)
 	test.Validate(result)
 
-	test = simply.Target(id.ExtendedString(), context, "Test microsoft UUID format")
+	test = simply.Target(id.ExtendedString(), t, "Test microsoft UUID format")
 	result = test.Equals(randomPrefix + expectedExtendedSuffix)
 	test.Validate(result)
+}
+
+func TestUUID_IsZero(t *testing.T) {
+	var id UUID
+	if !id.IsZero() {
+		t.Fatal("received non-zero value when zero value was expected")
+	}
+
+	id = New()
+
+	if id.IsZero() {
+		t.Fatal("received zero value when non-zero value was expected")
+	}
 }
 
 func BenchmarkUUID(b *testing.B) {
